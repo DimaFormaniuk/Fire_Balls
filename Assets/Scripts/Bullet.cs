1eb5a541ee6c976bbf30190583971e0174fe3468
DimaFormaniuk <Dinamikadze17@gmail.com>
2021-02-17 19:54:03 +0200

    Revert "init"
    This reverts commit e35ac19dd0237c172e5999aec9619d714770e4e2.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,9 +5,40 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _bounceForce;
+    [SerializeField] private float _bounceRadius;
 
-    private void FixedUpdate()
+    private Vector3 _moveDiraction;
+
+    private void Start()
     {
-        transform.position += Vector3.forward * _speed * Time.fixedDeltaTime;
+        _moveDiraction = Vector3.forward;
     }
+
+    private void Update()
+    {
+        transform.Translate(_moveDiraction * _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Block block))
+        {
+            block.Break();
+            Destroy(gameObject);
+        }
+        if (other.TryGetComponent(out Obstacle obstacle))
+        {
+            Bounce();
+        }
+    }
+
+    private void Bounce()
+    {
+        _moveDiraction = Vector3.back + Vector3.up;
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.isKinematic = false;
+        rigidbody.AddExplosionForce(_bounceForce, transform.position + new Vector3(0, -1, 1), _bounceRadius);
+    }
+
 }
